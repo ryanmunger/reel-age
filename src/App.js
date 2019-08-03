392dfa43aa69d85784e7b91d07caaf41160370ad
Ryan Munger <ryanmunger@Ryans-MacBook-Pro.local>
2019-08-03 17:39:58 -0400

    Initial wire up of POC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+import axios from 'axios';
+import { differenceInCalendarYears } from 'date-fns';
+
+const API_KEY = '7b25c97101de1db8db80fe1bfae994e0';
+
+const API = {
+  searchPersonUrl: `https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&append_to_response=movies`,
+  getMoviesUrl: (id) => `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=${API_KEY}&language=en-US`,
+  getActorUrl: (id) => `https://api.themoviedb.org/3/person/${id}?api_key=${API_KEY}&language=en-US`,
+};
+
+const handleOnChange = (setSearchResults, setActorDetails, setActorAge) => (e) => {
+  setActorDetails();
+  setActorAge();
+  if (e.target.value) {
+    axios.get(
+      `${API.searchPersonUrl}&query=${e.target.value}`
+    ).then((response) => {
+      console.log(response.data.results);
+      setSearchResults(response.data.results);
+    })
+  }
+  setSearchResults();
+}
+
+const handleOnClick = (id, setActorDetails, setSearchResults, setActorAge) => () => {
+  setSearchResults();
+  axios.get(
+    API.getMoviesUrl(id)
+  ).then((response) => {
+    console.log(response.data.cast);
+    setActorDetails(response.data.cast);
+  }).then(() => {
+    axios.get(
+      API.getActorUrl(id)
+    ).then(response => {
+      setActorAge(response.data.birthday);
+    })
+  })
+};
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState();
+  const [actorDetails, setActorDetails] = useState();
+  const [actorAge, setActorAge] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input onChange={handleOnChange(setSearchResults, setActorDetails, setActorAge)} />
+      {
+        searchResults && searchResults.map(searchResult => (
+          <div
+            onClick={handleOnClick(searchResult.id, setActorDetails, setSearchResults, setActorAge)}
+            key={searchResult.id}
+          >
+            {searchResult.name}
+          </div>
+        )
+        )}
+      {
+        actorDetails && actorDetails.map(actorDetail =>
+          <div key={actorDetail.id}>
+            {actorDetail.original_title} - <strong>{differenceInCalendarYears(actorDetail.release_date, actorAge)}</strong>
+          </div>
+        )
+      }
+    </>
   );
 }
 
